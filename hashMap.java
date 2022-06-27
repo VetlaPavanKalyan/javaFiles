94ab0f8f0d90e7e7cc07a13dba68656df971a826
VETLA PAVAN KALYAN <20131a4463@gmail.com>
2022-06-27 10:14:24 +0530

    Added to Repository

---
+++ hashMap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class hashMap {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Greeshmanth");
+        map.put(2, "SreeMouli");
+        map.put(3, "Tarun");
+        System.out.println(map);
+    }
+}
