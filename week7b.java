d4a0c668360f6c2ce95f725ac684210cbb02b30f
VETLA PAVAN KALYAN <20131a4463@gmail.com>
2022-07-03 10:36:47 +0530

    Commit on 3rd July 2022

--- week7b.java
+++ week7b.java
@@ -8,45 +8,35 @@ public class week7b {
         ArrayList<Integer> a = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
-        int op, i, ele;
         for (int j = 0; j <= 10; j++) {
             a.add(r.nextInt(10));
         }
         while (true) {
             System.out.println("OPTIONS : 1.ADD 2.GET 3.REMOVE 4.SET 5.CLEAR 6.SIZE 7.PRINT LIST 8.SORT 9.EXIT");
             System.out.println("Enter your Choice");
-            op = sc.nextInt();
-            switch (op) {
+            switch (sc.nextInt()) {
                 case 1 -> {
                     System.out.println("Enter the element : ");
-                    ele = sc.nextInt();
-                    a.add(ele);
+                    a.add(sc.nextInt());
                 }
                 case 2 -> {
                     System.out.println("Enter Index : ");
-                    i = sc.nextInt();
-                    System.out.println(a.get(i));
+                    System.out.println(a.get(sc.nextInt()));
                 }
                 case 3 -> {
                     System.out.println("Enter Index : ");
-                    i = sc.nextInt();
-                    a.remove(i);
+                    a.remove(sc.nextInt());
                 }
                 case 4 -> {
                     System.out.println("Enter the element : ");
-                    ele = sc.nextInt();
                     System.out.println("Enter Index : ");
-                    i = sc.nextInt();
-                    a.set(i, ele);
+                    a.set(sc.nextInt(), sc.nextInt());
                 }
                 case 5 -> {
                     a.clear();
                     System.out.println("List is Cleared!!!");
                 }
-                case 6 -> {
-                    ele = a.size();
-                    System.out.println("Size of the list is : " + ele);
-                }
+                case 6 -> System.out.println("Size of the list is : " + a.size());
                 case 7 -> {
                     for (int x : a) {
                         System.out.print(x + " ");
