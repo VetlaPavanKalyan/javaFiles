83a84a474bf793532aec970f94e70d8bdb853542
VETLA PAVAN KALYAN <20131a4463@gmail.com>
2022-07-03 10:05:27 +0530

    Commit on 3rd July 2022

--- week7b.java
+++ week7b.java
@@ -2,64 +2,62 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Collections;
+
 public class week7b {
     public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<Integer>();
+        ArrayList<Integer> a = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
-        int op,i,ele;
-        for(int j=0;j<=10;j++)
-        {
-            a.add(r.nextInt(10-0)+0);
+        int op, i, ele;
+        for (int j = 0; j <= 10; j++) {
+            a.add(r.nextInt(10));
         }
-        while(true){
+        while (true) {
             System.out.println("OPTIONS : 1.ADD 2.GET 3.REMOVE 4.SET 5.CLEAR 6.SIZE 7.PRINT LIST 8.SORT 9.EXIT");
             System.out.println("Enter your Choice");
             op = sc.nextInt();
-            switch (op){
-                case 1:
+            switch (op) {
+                case 1 -> {
                     System.out.println("Enter the element : ");
                     ele = sc.nextInt();
                     a.add(ele);
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Enter Index : ");
                     i = sc.nextInt();
                     System.out.println(a.get(i));
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     System.out.println("Enter Index : ");
                     i = sc.nextInt();
                     a.remove(i);
-                    break;
-                case 4:
+                }
+                case 4 -> {
                     System.out.println("Enter the element : ");
                     ele = sc.nextInt();
                     System.out.println("Enter Index : ");
                     i = sc.nextInt();
-                    a.set(i,ele);
-                    break;
-                case 5:
+                    a.set(i, ele);
+                }
+                case 5 -> {
                     a.clear();
                     System.out.println("List is Cleared!!!");
-                    break;
-                case 6:
+                }
+                case 6 -> {
                     ele = a.size();
-                    System.out.println("Size of the list is : "+ele);
-                    break;
-                case 7:
-                    for(int x : a){
-                        System.out.print(x+" ");
+                    System.out.println("Size of the list is : " + ele);
+                }
+                case 7 -> {
+                    for (int x : a) {
+                        System.out.print(x + " ");
                     }
-                    System.out.println("");
-                    break;
-                case 8:
-                    Collections.sort(a);
-                    break;
-                case 9:
+                    System.out.println();
+                }
+                case 8 -> Collections.sort(a);
+                case 9 -> {
                     System.out.println("Exited!!!");
                     System.exit(0);
-                    break;
+                }
             }
         }
     }
